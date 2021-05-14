f3331700092e1da10b7178a2f69d2574bf5c6ef6
Joel Jorge Nunes Vázquez <72249166+jnunesvazquez@users.noreply.github.com>
2021-05-14 10:16:28 +0200

    Ejercicio 3 (#3)
    * Ejercicio2.kt terminado con el array con numeros aleatorios
    
    * Ejercicio3.kt terminado con el array filtrado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,15 @@
 
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
-
-    for (iterador in Ejercicio1(11).array){
+    //Ejercicio 1
+   /* for (iterador in Ejercicio1(11).array){
         println(iterador)
-    }
+    }*/
+    //Ejercicio 2
+    /*val array = Ejercicio2(10){ Random.nextInt(50, 250)}
+    array.array.forEach { println(it) }*/
 
-    val array = Ejercicio2(10){ Random.nextInt(50, 250)}
-
-    array.array.forEach { println(it) }
+    //Ejercicio 3
+    val array2 = Ejercicio3().listaFiltrada(Ejercicio2(10, {Random.nextInt(50, 250)}).array)
 }
